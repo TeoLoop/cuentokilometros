caeaf71802911bf57676a3c0c8c33c8969566e63
Mateo Lopez <cuentokilometrosrenault@gmail.com>
2026-01-19 14:29:14 -0300

    [FEATURE] - Modificando voz y velocidad

--- lib/elevenlabs.ts
+++ lib/elevenlabs.ts
@@ -15,18 +15,19 @@ export async function generateAudio(text: string): Promise<Buffer> {
   try {
     // ID de la voz "Bill" (Narrador estilo documental/cuento)
     // Si prefieres a Rachel usa: "21m00Tcm4TlvDq8ikWAM" otro: "NDcVpQJv7Naa7ZKrqtEk" default "pqHfZKP75CvOlQylNhV4"
-    
-    const voiceId = "ByVRQtaK1WDOvTmP1PKO"; 
+
+    const voiceId = "gSYqSbtMajxq5LUT0bNl";
 
     const audioStream = await client.generate({
       voice: voiceId,
       text: text,
       model_id: "eleven_multilingual_v2", // Crucial para que hable bien español
       voice_settings: {
-        stability: 0.60,       // Más expresividad
+        stability: 0.6, // Más expresividad
         similarity_boost: 0.75, // Claridad de voz
         style: 0.0,
         use_speaker_boost: true,
+        speed: 1.15,
       },
     });
 
@@ -35,9 +36,8 @@ export async function generateAudio(text: string): Promise<Buffer> {
     for await (const chunk of audioStream) {
       chunks.push(Buffer.from(chunk));
     }
-    
-    return Buffer.concat(chunks);
 
+    return Buffer.concat(chunks);
   } catch (error) {
     console.error("Error generating audio with ElevenLabs:", error);
     throw error;
