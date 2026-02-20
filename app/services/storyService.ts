ed6ecc56843b0cb352753ceb7f95515df3bd208f
Mateo Lopez <cuentokilometrosrenault@gmail.com>
2026-02-20 15:03:05 -0300

    [DEPLOY] on my vercel

--- app/services/storyService.ts
+++ app/services/storyService.ts
@@ -31,6 +31,7 @@ export const generateAudioStream = async (text: string): Promise<Response> => {
     return res;
 };
 
+// Guardar email
 export const saveEmail = async (email: string): Promise<void> => {
     const res = await fetch("/api/save-email", {
         method: "POST",
